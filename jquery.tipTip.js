4c4559c13bd85f0a85b903fea2fecf74cbb290c7
Sebastian Daniel <daniel-spam@wobster.de>
2014-01-21 21:55:37 +0100

    svg support

--- jquery.tipTip.js
+++ jquery.tipTip.js
@@ -105,8 +105,8 @@
 					
 					var top = parseInt(org_elem.offset()['top']);
 					var left = parseInt(org_elem.offset()['left']);
-					var org_width = parseInt(org_elem.outerWidth());
-					var org_height = parseInt(org_elem.outerHeight());
+					var org_width = parseInt(org_elem[0].getBoundingClientRect().width);
+					var org_height = parseInt(org_elem[0].getBoundingClientRect().height);
 					var tip_w = tiptip_holder.outerWidth();
 					var tip_h = tiptip_holder.outerHeight();
 					var w_compare = Math.round((org_width - tip_w) / 2);
